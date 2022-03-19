1271aba4375c978a7cbb7427fed67a86381154b1
zoster2315 <zoster2315@gmail.com>
2022-03-20 00:48:33 +0400

    Открытие двери. Замена моделей, удаление старых перенаправлений.

---
+++ Source/Building_Escape/OpenDoor.cpp
@@ -0,0 +1,36 @@
+// Copyright Alexey Bekhmatov 2022
+
+
+#include "OpenDoor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FRotator OpenDoor = {0, 0, 0};
+
+	GetOwner()->SetActorRotation(OpenDoor);
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
