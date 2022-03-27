9a142d2f553365e9515e25bc9d62bd9d6952e28e
zoster2315 <zoster2315@gmail.com>
2022-03-27 13:39:06 +0400

    открытие двери по тригеру

--- Source/Building_Escape/OpenDoor.cpp
+++ Source/Building_Escape/OpenDoor.cpp
@@ -10,7 +10,7 @@ UOpenDoor::UOpenDoor()
     // off to improve performance if you don't need them.
     PrimaryComponentTick.bCanEverTick = true;
 
-    
+
     // ...
 }
 
@@ -21,6 +21,11 @@ void UOpenDoor::BeginPlay()
     Super::BeginPlay();
     OriginalYaw = GetOwner()->GetActorRotation().Yaw;
     TargetYaw = OriginalYaw + YawAngle;
+
+    if (!PressurePlate)
+    {
+        UE_LOG(LogTemp, Error, TEXT("%s has OpenDoor componetn but no PressurePlate set"), *GetOwner()->GetName());
+    }
 }
 
 
@@ -29,15 +34,14 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-    UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
-    UE_LOG(LogTemp, Warning, TEXT("Target Yaw is: %f"), TargetYaw);
-    UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+    if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpen))
+        OpenDoor(DeltaTime);
+}
 
+void UOpenDoor::OpenDoor(float DeltaTime) const
+{
     float CurrentYaw = GetOwner()->GetActorRotation().Yaw;
-    float NextYaw = FMath::FInterpConstantTo(CurrentYaw, TargetYaw, DeltaTime, 45); /*startingYaw, targetYaw*, 0-1*/
-    UE_LOG(LogTemp, Warning, TEXT("Target Yaw is: %f"), NextYaw);
-
+    float NextYaw = FMath::FInterpConstantTo(CurrentYaw, TargetYaw, DeltaTime, 45);
     FRotator OpenDoor(0, NextYaw, 0);
     GetOwner()->SetActorRotation(OpenDoor);
 }
-
--- Source/Building_Escape/OpenDoor.h
+++ Source/Building_Escape/OpenDoor.h
@@ -4,15 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BUILDING_ESCAPE_API UOpenDoor : public UActorComponent
 {
     GENERATED_BODY()
 
-public:    
+public:
     // Sets default values for this component's properties
     UOpenDoor();
 
@@ -20,14 +21,19 @@ protected:
     // Called when the game starts
     virtual void BeginPlay() override;
 
-public:    
+public:
     // Called every frame
     virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+    void OpenDoor(float DeltaTime) const;
 
 private:
     float OriginalYaw = 0.f;
     UPROPERTY(EditAnywhere)
-    float YawAngle = -90.f;
+        float YawAngle = -90.f;
     float TargetYaw = 0.f;
-        
+    UPROPERTY(EditAnywhere)
+        ATriggerVolume* PressurePlate;
+
+    UPROPERTY(EditAnywhere)
+        APawn* ActorThatOpen;
 };
